b3d4bea0cf7a6d3494c06807ba5eea2f70000d95
unknown <devsmith0213@gmail.com>
2020-10-25 02:41:26 +0800

    2thUpdate

--- src/Screens/TarbarScreens/ProgramWorkoutDetailScreen.js
+++ src/Screens/TarbarScreens/ProgramWorkoutDetailScreen.js
@@ -41,7 +41,7 @@ export default class ProgramDetailStartScreen extends Component {
                                         <Text style={styles.itemTxt}>Based</Text>
                                     </View>
                                 </View>
-                                <TouchableOpacity style={styles.createBtn2} onPress={() => this.props.navigation.navigate("CreateScreen")}>
+                                <TouchableOpacity style={styles.createBtn2}>
                                     <Text style={styles.CreateTxt}>Start Workout</Text>
                                 </TouchableOpacity>
                             </ImageBackground>
